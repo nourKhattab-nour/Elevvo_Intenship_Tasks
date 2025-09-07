2ead7db878b19f5685c5d9e33dbe009440d7bc2c
nourKhattab-nour <nour.khattab90@gmail.com>
2025-09-07 19:39:06 +0300

    Redeploy

--- task-four/vite.config.js
+++ task-four/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/Elevvo_Intenship_Tasks/task-four/", // 👈 VERY important
+  base: "/Elevvo_Intenship_Tasks/", 
   plugins: [react()],
 });

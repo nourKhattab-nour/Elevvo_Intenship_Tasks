b4b7704342e7370f01011cf41b026e1f771346ea
nourKhattab-nour <nour.khattab90@gmail.com>
2025-09-07 19:21:56 +0300

    Fix vite and add the gitPages

--- task-four/vite.config.js
+++ task-four/vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/Elevvo_Intenship_Tasks/task-four/", // 👈 VERY important
   plugins: [react()],
-})
+});
